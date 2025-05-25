aa7840c80321cef288b8c7aa91f4a6fbfd6b7134
Van Luu <thanhvanluu.3199@gmail.com>
2025-05-25 15:49:16 +0700

    Support displaying custom svg icons

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
@@ -6,5 +8,21 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "CRM-app";
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.loadIcons();
+  }
+
+  loadIcons() {
+    const iconLabels = ["crm"];
+    for (let icon of iconLabels) {
+      const path = "../assets/images/icons/icon-";
+      this.matIconRegistry.addSvgIcon(
+        icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`${path}${icon}.svg`)
+      );
+    }
+  }
 }
